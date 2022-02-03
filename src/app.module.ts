657da4b254559629ec5e547ac47be0fee8fce246
mccreate <minwoo6951@naver.com>
2022-02-03 13:29:11 +0900

    MongoDB connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
+
 config();
 
 console.log(process.env.MONGO_URL);
+
 @Module({
   imports: [MoviesModule, MongooseModule.forRoot(process.env.MONGO_URL)],
   controllers: [AppController],
--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,6 +1,8 @@
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateMovieDTO {
+  @IsNumber()
+  readonly id: number;
   @IsString()
   readonly title: string;
   @IsNumber()
